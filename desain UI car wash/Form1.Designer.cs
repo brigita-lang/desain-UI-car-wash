b889f1cff37ccff62e476c957ebe126f9a591dd1
DELL <DELL@DESKTOP-N5KTVL3>
2024-10-01 13:11:44 +0800

    Add project files.

---
+++ desain UI car wash/Form1.Designer.cs
@@ -0,0 +1,188 @@
+﻿namespace desain_UI_car_wash
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNamaPelanggan = new System.Windows.Forms.TextBox();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.label5 = new System.Windows.Forms.Label();
+            this.dateTimePicker1 = new System.Windows.Forms.DateTimePicker();
+            this.btnCancel = new System.Windows.Forms.Button();
+            this.btnProceed = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(243, 21);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(299, 26);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Clean Car Booking System";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(56, 103);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(162, 22);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Nama Pelanggan";
+            this.label2.Click += new System.EventHandler(this.label2_Click);
+            // 
+            // txtNamaPelanggan
+            // 
+            this.txtNamaPelanggan.Location = new System.Drawing.Point(258, 103);
+            this.txtNamaPelanggan.Name = "txtNamaPelanggan";
+            this.txtNamaPelanggan.Size = new System.Drawing.Size(197, 26);
+            this.txtNamaPelanggan.TabIndex = 2;
+            this.txtNamaPelanggan.TextChanged += new System.EventHandler(this.txtNamaPelanggan_TextChanged);
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Location = new System.Drawing.Point(258, 156);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 28);
+            this.comboBox1.TabIndex = 3;
+            this.comboBox1.SelectedIndexChanged += new System.EventHandler(this.comboBox1_SelectedIndexChanged);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.Location = new System.Drawing.Point(56, 162);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(160, 22);
+            this.label3.TabIndex = 4;
+            this.label3.Text = "Jenis Kendaraan";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label4.Location = new System.Drawing.Point(56, 215);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(156, 22);
+            this.label4.TabIndex = 5;
+            this.label4.Text = "Jenis Pencucian";
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(258, 209);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(180, 28);
+            this.comboBox2.TabIndex = 6;
+            this.comboBox2.SelectedIndexChanged += new System.EventHandler(this.comboBox2_SelectedIndexChanged);
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label5.Location = new System.Drawing.Point(55, 273);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(161, 22);
+            this.label5.TabIndex = 7;
+            this.label5.Text = "Tanggal Booking";
+            // 
+            // dateTimePicker1
+            // 
+            this.dateTimePicker1.Location = new System.Drawing.Point(258, 269);
+            this.dateTimePicker1.Name = "dateTimePicker1";
+            this.dateTimePicker1.Size = new System.Drawing.Size(200, 26);
+            this.dateTimePicker1.TabIndex = 8;
+            // 
+            // btnCancel
+            // 
+            this.btnCancel.Location = new System.Drawing.Point(482, 359);
+            this.btnCancel.Name = "btnCancel";
+            this.btnCancel.Size = new System.Drawing.Size(102, 34);
+            this.btnCancel.TabIndex = 9;
+            this.btnCancel.Text = "Cancel";
+            this.btnCancel.UseVisualStyleBackColor = true;
+            // 
+            // btnProceed
+            // 
+            this.btnProceed.Location = new System.Drawing.Point(625, 359);
+            this.btnProceed.Name = "btnProceed";
+            this.btnProceed.Size = new System.Drawing.Size(104, 34);
+            this.btnProceed.TabIndex = 10;
+            this.btnProceed.Text = "Proceed";
+            this.btnProceed.UseVisualStyleBackColor = true;
+            this.btnProceed.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Bisque;
+            this.ClientSize = new System.Drawing.Size(800, 501);
+            this.Controls.Add(this.btnProceed);
+            this.Controls.Add(this.btnCancel);
+            this.Controls.Add(this.dateTimePicker1);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.txtNamaPelanggan);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNamaPelanggan;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.ComboBox comboBox2;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.DateTimePicker dateTimePicker1;
+        private System.Windows.Forms.Button btnCancel;
+        private System.Windows.Forms.Button btnProceed;
+    }
+}
+
---
+++ desain UI car wash/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using static System.Windows.Forms.VisualStyles.VisualStyleElement;
+
+namespace desain_UI_car_wash
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void label2_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show("Apakah Anda Ingin Melanjutkan Proses ini", "Warning", MessageBoxButtons.OK, MessageBoxIcon.Warning);
+
+           
+                Form3 form3 = new Form3();
+                form3.Show();
+                this.Hide();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            comboBox1.Items.Add("Mobil");
+            comboBox1.Items.Add("Motor");
+        }
+
+      
+        private void comboBox1_SelectedIndexChanged(object sender, EventArgs e)
+        {
+           
+            comboBox2.Items.Clear();
+
+           
+            if (comboBox1.SelectedItem.ToString() == "Mobil")
+            {
+                comboBox2.Items.Add("Cuci Manual (Rp 30.000 - Rp 60.000)");
+                comboBox2.Items.Add("Cuci Hidrolik (Rp 50.000 - Rp 100.000)");
+                comboBox2.Items.Add("Cuci Salju (Rp 50.000 - Rp 120.000)");
+                comboBox2.Items.Add("Cuci Detailing (Rp 200.000 - Rp 1.000.000)");
+                comboBox2.Items.Add("Cuci Steam (Rp 100.000 - Rp 200.000)");
+                comboBox2.Items.Add("Cuci Premium (Rp 300.000 - Rp 2.000.000)");
+                comboBox2.Items.Add("Cuci Express (Rp 20.000 - Rp 40.000)");
+            }
+            else if (comboBox2.SelectedItem.ToString() == "Motor")
+            {
+                comboBox2.Items.Add("Cuci Manual (Rp 10.000 - Rp 30.000)");
+                comboBox2.Items.Add("Cuci Hidrolik (Rp 20.000 - Rp 40.000)");
+                comboBox2.Items.Add("Cuci Salju (Rp 20.000 - Rp 40.000)");
+                comboBox2.Items.Add("Cuci Detailing (Rp 50.000 - Rp 200.000)");
+                comboBox2.Items.Add("Cuci Steam (Rp 30.000 - Rp 60.000)");
+            }
+        }
+
+        private void txtNamaPelanggan_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+
+        private void comboBox2_SelectedIndexChanged(object sender, EventArgs e)
+        {
+
+        }
+    }
+}
